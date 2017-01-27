2436247080590ae9bed8359ba32725c63865d224
apiepho <al@Al-iMac2.local>
2017-01-27 15:50:37 -0700

    first pass of all stats pages

--- scrapy_gc/spiders/gc.py
+++ scrapy_gc/spiders/gc.py
@@ -94,20 +94,16 @@ class GameChanger(Spider):
         sel = Selector(response)
         list = sel.css('ul[id=menu]')
         links = list.css('a')
-        links = links[21:22]  # just roos for debug
+        links = links[21:22]  # DEBUG: just roos for debug
         for link in links:
             href = link.css('a::attr(href)').extract_first()
             if href.find('/t/') != -1:
                 next_page = self.base_url + href
-                #yield scrapy.Request(next_page, callback=self.parse_team)
-                yield scrapy.Request(next_page, callback=self.parse_team, meta={
-                	'splash': {
-                		'args': {
-                			'wait': 0.0,
-                			'html': 1
-                		}
-                	}
-                })
+                yield scrapy.Request(
+                	next_page,
+                	callback=self.parse_team,
+                	meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+                )
                 #return
         pass
        
@@ -116,64 +112,188 @@ class GameChanger(Spider):
         print "TEAM: " + response.url
         self.cache_name(response.url)
         self.cache_page(response.body)
-        #print response.body
-
-        sel = Selector(response)
-        
+       
+        # get roster
+        next_page = response.url + '/roster'
+        yield scrapy.Request(
+            next_page,
+            callback=self.parse_team_roster,
+            meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+        )
+                
         # process game links
+        sel = Selector(response)
         list = sel.css('li[class=newsFeedItem]')
         links = list.css('a')
         for link in links:
             href = link.css('a::attr(href)').extract_first()
-            if href.find('/stats') != -1:
+            if href.find('/recap-story') != -1:
                 next_page = self.base_url + href
-                yield scrapy.Request(next_page, callback=self.parse_game_stats, meta={
-                	'splash': {
-                		'args': {
-                			'wait': 0.0,
-                			'html': 1
-                		}
-                	}
-                })
+                next_page = next_page.replace( 'recap-story', 'stats')
+                yield scrapy.Request(
+                	next_page,
+                	callback=self.parse_game_stats,
+                	meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+                )
+                next_page = next_page.replace( 'stats', 'plays')
+                yield scrapy.Request(
+                	next_page,
+                	callback=self.parse_game_stats,
+                	meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+                )
                 #return
         pass
+
         
     # handle response of each game stat page
     def parse_game_stats(self, response):
-        print "GAME STAT: " + response.url
+        print "GAME STATS: " + response.url
         self.cache_name(response.url)
         self.cache_page(response.body)
         pass
  
-    # handle response of each game stat page
-    def parse_game_recap(self, response):
-        print "GAME RECAP: " + response.url
+    # handle response of each game plays page
+    def parse_game_plays(self, response):
+        print "GAME PLAYS: " + response.url
         self.cache_name(response.url)
         self.cache_page(response.body)
         pass
-       
-    '''
-GC_PLAYS_URI                      = GC_BASE_URI + "/game-%s/plays"
-GC_SCOREBOOK_URI                  = GC_BASE_URI + "/game-%s/scorebook"
 
-GC_ROSTER_URI                     = "%s/roster"                # given team guid
-GC_PLAYER_URI                     = "%s/p/%s"                  # given team guid and player fname-lastinitial-guid
-GC_RECAP_URI                      = "%s/game-%s/recap-story"   # given base and game guid
+    # handle response of each team roster page
+    def parse_team_roster(self, response):
+        print "TEAM ROSTER: " + response.url
+        self.cache_name(response.url)
+        self.cache_page(response.body)
 
-GC_STATS_URI                      = "%s/game-%s/stats"         # given base and game guid
+        # get player pages
+        sel = Selector(response)
+        tr_list = sel.css('tr')
+        links = tr_list.css('a')
+        #links = list(set(links))  # use set to get unique list
+        for link in links:
+            name = link.css('span::text').extract_first()
+            if name.isnumeric() == True:
+            	continue
+            	
+            # need to build player string like <first name>-<last initial>-<id>
+            parts = name.split(' ')
+            fname = parts[0].lower()
+            lname = parts[1].lower()
+            lname = lname[:1]
+            id = link.css('a::attr(href)').extract_first().replace('/player-', '')
+            pstr = "/p/%s-%s-%s" % (fname, lname, id)
 
-# parameters for the following: [GC_BASE_URI, fteam, team_id, fname, linitial, player_id]
-GC_PLAYER_BATTING_STANDARD_URI    = "%s/t/%s-%s/p/%s-%s-%s/batting/standard"
-GC_PLAYER_BATTING_SPEED_URI       = "%s/t/%s-%s/p/%s-%s-%s/batting/expanded"
-GC_PLAYER_BATTING_TEAMIMPACT_URI  = "%s/t/%s-%s/p/%s-%s-%s/batting/expanded2"
-GC_PLAYER_PITCHING_STANDARD_URI   = "%s/t/%s-%s/p/%s-%s-%s/pitching/standard"
-GC_PLAYER_PITCHING_EFFICIENCY_URI = "%s/t/%s-%s/p/%s-%s-%s/pitching/expanded"
-GC_PLAYER_PITCHING_COMMAND_URI    = "%s/t/%s-%s/p/%s-%s-%s/pitching/expanded2"
-GC_PLAYER_PITCHING_BATTER_URI     = "%s/t/%s-%s/p/%s-%s-%s/pitching/expanded3"
-GC_PLAYER_PITCHING_RUNS_URI       = "%s/t/%s-%s/p/%s-%s-%s/pitching/expanded4"
-GC_PLAYER_PITCHING_PITCH_URI      = "%s/t/%s-%s/p/%s-%s-%s/pitching/expanded5"
-GC_PLAYER_FIELDING_STANDARD_URI   = "%s/t/%s-%s/p/%s-%s-%s/fielding/standard"
-GC_PLAYER_FIELDING_CATCHING_URI   = "%s/t/%s-%s/p/%s-%s-%s/fielding/expanded"
-GC_PLAYER_BATTING_SPRAY_URI       = "%s/t/%s-%s/p/%s-%s-%s/spray-chart"
-    '''
+            # response url is like
+            # https://gc.com/t/spring-2016/roosevelt-rough-riders-varsity-56dfa90020277d0024b46bbb/roster
+            # need
+            # https://gc.com/t/roosevelt-56dfa90020277d0024b46bbb
+
+            # get parts for /
+            next_page = response.url
+            parts = next_page.split('/')
+
+            # fix teamname-id part
+            parts2 = parts[5].split('-')
+            parts[5] = parts2[0] + '-' + parts2[-1]
+
+            # remove unwanted
+            del parts[6]  # remove roster
+            del parts[4]  # remove spring-2016
+  
+            # put them back together          
+            next_page  = '/'.join(parts)
+            next_page  = next_page + pstr
+            saved_page = next_page
+
+            next_page = saved_page + '/batting/standard'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/batting/expanded'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/batting/expanded2'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/standard'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/expanded'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/expanded2'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/expanded3'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/expanded4'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/pitching/expanded5'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/fielding/standard'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/fielding/expanded'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+
+            next_page = saved_page + '/spray-chart'
+            yield scrapy.Request(
+                next_page,
+                callback=self.parse_player_stat,
+                meta={'splash': {'args': {'wait': 0.0, 'html': 1}}}
+            )
+        pass
+
+    # handle response of each player stat page
+    def parse_player_stat(self, response):
+        print "PLAYER STAT: " + response.url
+        self.cache_name(response.url)
+        self.cache_page(response.body)
+        pass
 
